11f3b387ac478428814272e42c7c2f300902c0d1
mountrivers <36880919+mountrivers@users.noreply.github.com>
2020-03-06 14:06:48 +0900

    개발자/라이선스 고지 위한 엑티비티 생성

---
+++ app/src/main/java/com/sanha/overlaymemo/HELP/SeeMoreActivity.java
@@ -0,0 +1,16 @@
+package com.sanha.overlaymemo.HELP;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sanha.overlaymemo.R;
+
+public class SeeMoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_see_more);
+    }
+}
